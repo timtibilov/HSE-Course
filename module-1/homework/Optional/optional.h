c7992e67e035e111f761a88aa01d21b7747d25ba
t.tibilov <t.tibilov@macbook-t.tibilov>
2021-06-07 10:35:56 +0300

    Optional HW is done

--- module-1/homework/Optional/optional.h
+++ module-1/homework/Optional/optional.h
@@ -5,60 +5,86 @@
 
 namespace task {
 
-struct NullOpt {
-    // Your code goes here;
-};
+    struct NullOpt {
+        NullOpt() noexcept = default;
+    };
 
-constexpr NullOpt kNullOpt = // Your code goes here;
+    constexpr NullOpt kNullOpt = {};
 
-struct InPlace {
-    // Your code goes here;
-};
+    struct InPlace {
+        InPlace() noexcept = default;
+    };
 
-constexpr InPlace kInPlace =  //Your code goes here;
+    constexpr InPlace kInPlace = {};
 
-template <typename T>
-class Optional : public // Your code goes here; {
-public:
-    using value_type =  // Your code goes here;
+    template <typename T>
+    class Optional {
+    public:
+        using value_type = T;
+        using pointer = std::add_pointer_t<value_type>;
 
-    constexpr Optional() noexcept;
+        constexpr Optional() noexcept : ptr_(nullptr) {}
 
-    template <typename U = value_type>
-    constexpr explicit Optional(U&& value);
+        template <typename U = value_type>
+        constexpr explicit Optional(U&& value) : ptr_(new value_type(std::forward<U>(value))) {}
 
-    constexpr explicit Optional(NullOpt) noexcept;
+        constexpr explicit Optional(NullOpt) noexcept : ptr_(nullptr) {}
 
-    template <typename... Args>
-    constexpr explicit Optional(InPlace, Args&&... args);
+        template <typename... Args>
+        constexpr explicit Optional(InPlace, Args&&... args) : ptr_(new value_type(std::forward<Args>(args)...)) {}
 
-    Optional& operator=(NullOpt) noexcept;
+        Optional& operator=(NullOpt) noexcept;
 
-    template <typename U = T>
-    Optional& operator=(U&& value);
+        template <typename U = T>
+        Optional& operator=(U&& value);
 
-    void Reset() noexcept;
+        void Reset() noexcept;
 
-    template <typename U>
-    constexpr T ValueOr(U&& default_value) const&;
+        template <typename U>
+        constexpr T ValueOr(U&& default_value) const& { return HasValue() ? *ptr_ : default_value; }
 
-    template <typename U>
-    constexpr T ValueOr(U&& default_value) &&;
+        template <typename U>
+        constexpr T ValueOr(U&& default_value) && { return HasValue() ? *ptr_ : default_value; }
 
-    constexpr bool HasValue() const noexcept;
+        constexpr bool HasValue() const noexcept { return ptr_ != nullptr; }
 
-    constexpr explicit operator bool() const noexcept;
+        constexpr explicit operator bool() const noexcept { return ptr_ != nullptr; }
 
-    constexpr std::add_pointer_t<const value_type> operator->() const;
+        constexpr std::add_pointer_t<const value_type> operator->() const { return ptr_; }
 
-    constexpr std::add_pointer_t<value_type> operator->();
+        constexpr std::add_pointer_t<value_type> operator->() { return ptr_; }
 
-    constexpr const value_type& operator*() const&;
+        constexpr const value_type& operator*() const& { return *ptr_; }
 
-    constexpr value_type& operator*() &;
+        constexpr value_type& operator*() & { return *ptr_; }
 
-    constexpr const value_type&& operator*() const&&;
+        constexpr const value_type&& operator*() const&& { return *ptr_; }
 
-    constexpr value_type&& operator*() &&;
-};
+        constexpr value_type&& operator*() && { return *ptr_; }
+
+    private:
+        using is_triv_dest = std::is_trivially_destructible<T>;
+        pointer ptr_;
+
+    };
 }  // namespace task
+
+template<typename T>
+task::Optional<T>& task::Optional<T>::operator=(task::NullOpt) noexcept {
+    Reset();
+    return *this;
+}
+
+template<typename T>
+template<typename U>
+task::Optional<T>& task::Optional<T>::operator=(U&& value) {
+    Reset();
+    ptr_(new task::Optional<T>::value_type(std::forward<U>(value)));
+    return *this;
+}
+
+template<typename T>
+void task::Optional<T>::Reset() noexcept {
+    delete ptr_;
+    ptr_ = nullptr;
+}
