dae1a15aa1b8126f1d51b7a533f3f7a665435ee5
t.tibilov <t.tibilov@macbook-t.tibilov>
2021-06-09 13:32:29 +0300

    Implemented Control, Shared and Weak pointers, changed naming and CMakeFiles

--- module-1/homework/SharedPointer/src/shared_ptr/shared_ptr.h
+++ module-1/homework/SharedPointer/src/shared_ptr/shared_ptr.h
@@ -9,16 +9,18 @@ class WeakPtr;
 template <typename T>
 class SharedPtr {
 public:
-    using element_type =     // Your code goes here...
+    using element_type = std::remove_extent_t<T>;
+    using pointer = element_type*;
+    using weak_type = WeakPtr<T>;
 
     constexpr SharedPtr() noexcept = default;
     ~SharedPtr();
 
     template <typename Y>
-    explicit SharedPtr(Y* p);
+    explicit SharedPtr(Y* p) : ptr_(p), control_(new ControlBlock<pointer>(p)) {}
 
     template <typename Y, typename Deleter>
-    SharedPtr(Y* p, Deleter deleter) noexcept;
+    SharedPtr(Y* p, Deleter deleter) noexcept : ptr_(p), control_(new ControlBlock<pointer, Deleter>(p, deleter)) {}
 
     SharedPtr(const SharedPtr& other) noexcept;
     SharedPtr(SharedPtr&& other) noexcept;
@@ -34,38 +36,114 @@ public:
     SharedPtr& operator=(SharedPtr<Y>&& r) noexcept;
 
     // Modifiers
-    void Reset() noexcept;
+    void reset() noexcept;
 
     template <typename Y>
-    void Reset(Y* p) noexcept;
+    void reset(Y* p) noexcept;
 
     template <typename Y, typename Deleter>
-    void Reset(Y* p, Deleter deleter) noexcept;
+    void reset(Y* p, Deleter deleter) noexcept;
 
-    void Swap(SharedPtr& other) noexcept;
+    void swap(SharedPtr& other) noexcept;
 
     // Observers
-    T* Get() const noexcept;
-    int64_t UseCount() const noexcept;
-    T& operator*() const noexcept;
-    T* operator->() const noexcept;
-    element_type& operator[](std::ptrdiff_t idx) const;
+    T* get() const noexcept { return ptr_; }
+    std::size_t useCount() const noexcept;
+    T& operator*() const noexcept { return *ptr_; }
+    T* operator->() const noexcept { return ptr_; }
+    element_type& operator[](std::ptrdiff_t idx) const { return *(ptr_ + idx); }
     explicit operator bool() const noexcept;
 
     template <typename U>
     friend class WeakPtr;
 
 private:
-    // Your code goes here...
+    pointer ptr_ = nullptr;
+    SharedWeakCount* control_ = nullptr;
 };
 
 
 // MakeShared
-// Your code goes here...
+template<typename T, typename... Args>
+SharedPtr<T> makeShared(Args&&... args) {
+    return SharedPtr<T>(new T(std::forward<Args>(args)...));
+}
 // MakeShared
 
 // SharedPtr
-// Your code goes here...
+template<typename T>
+SharedPtr<T>::~SharedPtr() {
+    if (control_ != nullptr) {
+        control_->releaseShared();
+    }
+    ptr_ = nullptr;
+    control_ = nullptr;
+}
+
+template<typename T>
+SharedPtr<T>::SharedPtr(const SharedPtr& other) noexcept :
+    ptr_(other.ptr_),
+    control_(other.control_)
+{
+    control_->retainShared();
+}
+
+template<typename T>
+SharedPtr<T>::SharedPtr(SharedPtr&& other) noexcept :
+    ptr_(other.ptr_),
+    control_(other.control_)
+{
+    other.ptr_ = nullptr;
+    other.control_ = nullptr;
+}
+
+template<typename T>
+SharedPtr<T>& SharedPtr<T>::operator=(const SharedPtr& r) noexcept {
+    SharedPtr<T>(r).swap(*this);
+    return *this;
+}
+
+template<typename T>
+SharedPtr<T>& SharedPtr<T>::operator=(SharedPtr&& r) noexcept {
+    SharedPtr<T>(std::forward<SharedPtr>(r)).swap(*this);
+    return *this;
+}
+
+template<typename T>
+void SharedPtr<T>::reset() noexcept {
+    ptr_ = nullptr;
+    if (control_ != nullptr) {
+        control_->releaseShared();
+    }
+}
+
+template<typename T>
+template<typename Y>
+void SharedPtr<T>::reset(Y* p) noexcept {
+    SharedPtr<T>(p).swap(*this);
+}
+
+template<typename T>
+template<typename Y, typename Deleter>
+void SharedPtr<T>::reset(Y* p, Deleter deleter) noexcept {
+    SharedPtr<T>(p, deleter).swap(*this);
+}
+
+template<typename T>
+void SharedPtr<T>::swap(SharedPtr &other) noexcept {
+    std::swap(control_, other.control_);
+    std::swap(ptr_, other.ptr_);
+}
+
+template<typename T>
+std::size_t SharedPtr<T>::useCount() const noexcept {
+    return control_ == nullptr ? 0 : control_->useCount();
+}
+
+template<typename T>
+SharedPtr<T>::operator bool() const noexcept {
+    return useCount() > 0 && ptr_ != nullptr;
+}
 // SharedPtr
 
 // WeakPtr
@@ -73,7 +151,8 @@ template <typename T>
 class WeakPtr {
 
 public:
-    using element_type =     // Your code goes here...
+    using element_type = std::remove_extent_t<T>;
+    using pointer = T*;
 
     // Special-member functions
     constexpr WeakPtr() noexcept = default;
@@ -89,20 +168,122 @@ public:
     ~WeakPtr();
 
     // Modifiers
-    void Reset() noexcept;
-    void Swap(WeakPtr<T>& other) noexcept;
+    void reset() noexcept;
+    void swap(WeakPtr<T>& other) noexcept;
 
     // Observers
-    bool Expired() noexcept;
-    SharedPtr<T> Lock() const noexcept;
+    bool expired() noexcept;
+    SharedPtr<T> lock() const noexcept;
 
     template <typename U>
     friend class SharedPtr;
 
-public:
-    // Your code goes here...
+private:
+    pointer ptr_ = nullptr;
+    SharedWeakCount* control_ = nullptr;
 };
 
 // WeakPtr
-// Your code goes here...
+
+template<typename T>
+template<typename Y>
+WeakPtr<T>::WeakPtr(const SharedPtr<Y>& other) :
+    ptr_(other.ptr_),
+    control_(other.control_)
+{
+    if (control_ != nullptr)
+        control_->retainWeak();
+}
+
+template<typename T>
+WeakPtr<T>::WeakPtr(const WeakPtr& other) noexcept :
+    ptr_(other.ptr_),
+    control_(other.control_)
+{
+    if (control_ != nullptr)
+        control_->retainWeak();
+}
+
+template<typename T>
+WeakPtr<T>::WeakPtr(WeakPtr&& other) noexcept :
+    ptr_(other.ptr_),
+    control_(other.control_)
+{
+    other.ptr_ = nullptr;
+    other.control_ = nullptr;
+}
+
+template<typename T>
+template<typename Y>
+WeakPtr<T>& WeakPtr<T>::operator=(const SharedPtr<Y>& other) {
+    reset();
+    control_ = other.control_;
+    ptr_ = other.ptr_;
+    if (control_ != nullptr) {
+        control_->retainWeak();
+    }
+    return *this;
+}
+
+template<typename T>
+WeakPtr<T>& WeakPtr<T>::operator=(const WeakPtr& other) noexcept {
+    if (this == other) {
+        return *this;
+    }
+    reset();
+    ptr_ = other.ptr_;
+    control_ = other.control_;
+    if (control_ != nullptr) {
+        control_->retainWeak();
+    }
+    return *this;
+}
+
+template<typename T>
+WeakPtr<T>& WeakPtr<T>::operator=(WeakPtr&& other) noexcept {
+    reset();
+    ptr_ = other.ptr_;
+    control_ = other.control_;
+    other.ptr_ = nullptr;
+    other.control_ = nullptr;
+    return *this;
+}
+
+template<typename T>
+WeakPtr<T>::~WeakPtr() {
+    if (control_ != nullptr) {
+        control_->releaseWeak();
+    }
+}
+
+template<typename T>
+void WeakPtr<T>::reset() noexcept {
+    if (control_ != nullptr && ptr_ != nullptr) {
+        ptr_ = nullptr;
+        control_->releaseWeak();
+        control_ = nullptr;
+    }
+}
+
+template<typename T>
+void WeakPtr<T>::swap(WeakPtr<T>& other) noexcept {
+    std::swap(ptr_, other.ptr_);
+    std::swap(control_, other.control_);
+}
+
+template<typename T>
+bool WeakPtr<T>::expired() noexcept {
+    return control_ == nullptr || control_->useCount() <= 0l;
+}
+
+template<typename T>
+SharedPtr<T> WeakPtr<T>::lock() const noexcept {
+    SharedPtr<T> ptr;
+    ptr.control_ = control_ != nullptr ? control_->lock() : control_;
+    if (ptr.control_ != nullptr) {
+        ptr.ptr_ = this->ptr_;
+    }
+    return ptr;
+}
+
 // WeakPtr
--- module-1/homework/SharedPointer/tests.cpp
+++ module-1/homework/SharedPointer/tests.cpp
@@ -8,127 +8,127 @@
 TEST(WeakExpired, Test1) {
     WeakPtr<int32_t> w;
     {
-        auto sp = MakeShared<int32_t>(42);
+        auto sp = makeShared<int32_t>(42);
         w = sp;
-        ASSERT_FALSE(w.Expired());
+        ASSERT_FALSE(w.expired());
     }
 }
 
 TEST(WeakExpired, Test2) {
     WeakPtr<int32_t> w;
     {
-        auto sp = MakeShared<int32_t>(42);
+        auto sp = makeShared<int32_t>(42);
         w = sp;
     }
-    ASSERT_TRUE(w.Expired());
+    ASSERT_TRUE(w.expired());
 }
 
 TEST(WeakReset, Test3) {
     WeakPtr<int32_t> w;
-    auto sp = MakeShared<int32_t>(42);
+    auto sp = makeShared<int32_t>(42);
     w = sp;
-    w.Reset();
-    ASSERT_TRUE(w.Expired());
+    w.reset();
+    ASSERT_TRUE(w.expired());
 }
 
 TEST(WeakLock, Test1) {
     WeakPtr<int32_t> w;
-    auto sp = MakeShared<int32_t>(42);
+    auto sp = makeShared<int32_t>(42);
     w = sp;
-    ASSERT_TRUE(*w.Lock() == 42);
+    ASSERT_TRUE(*w.lock() == 42);
 }
 
 TEST(WeakLock, Test2) {
     WeakPtr<int32_t> w;
     {
-        auto sp = MakeShared<int32_t>(42);
+        auto sp = makeShared<int32_t>(42);
         w = sp;
     }
-    ASSERT_FALSE(w.Lock());
+    ASSERT_FALSE(w.lock());
 }
 
 // SharedPtr
 TEST(SharedMoveConstructor, Test1) {
     class Contrainer {};
-    SharedPtr<Contrainer> s1 = MakeShared<Contrainer>();
-    Contrainer* raw_s1 = s1.Get();
+    SharedPtr<Contrainer> s1 = makeShared<Contrainer>();
+    Contrainer* raw_s1 = s1.get();
     SharedPtr<Contrainer> s2 = std::move(s1);
-    ASSERT_TRUE(s1.UseCount() == 0 && s1.Get() == nullptr && s2.Get() == raw_s1 &&
-                s2.UseCount() == 1);
+    ASSERT_TRUE(s1.useCount() == 0 && s1.get() == nullptr && s2.get() == raw_s1 &&
+                s2.useCount() == 1);
 }
 
 TEST(SharedMoveAssignment, Test1) {
     class Contrainer {};
-    SharedPtr<Contrainer> s1 = MakeShared<Contrainer>();
-    Contrainer* raw_s1 = s1.Get();
+    SharedPtr<Contrainer> s1 = makeShared<Contrainer>();
+    Contrainer* raw_s1 = s1.get();
     SharedPtr<Contrainer> s2;
     s2 = std::move(s1);
-    ASSERT_TRUE(s1.UseCount() == 0 && s1.Get() == nullptr && s2.Get() == raw_s1 &&
-                s2.UseCount() == 1);
+    ASSERT_TRUE(s1.useCount() == 0 && s1.get() == nullptr && s2.get() == raw_s1 &&
+                s2.useCount() == 1);
 }
 
 TEST(SharedAssignment, Test1) {
     class Contrainer {};
-    SharedPtr<Contrainer> s1 = MakeShared<Contrainer>();
+    SharedPtr<Contrainer> s1 = makeShared<Contrainer>();
     SharedPtr<Contrainer> s2;
     SharedPtr<Contrainer> s3;
     s2 = s1;
     s3 = s2;
 
-    ASSERT_TRUE(s1.UseCount() == 3 && s2.UseCount() == 3 && s3.UseCount() == 3 &&
-                s1.Get() == s2.Get() && s2.Get() == s3.Get());
+    ASSERT_TRUE(s1.useCount() == 3 && s2.useCount() == 3 && s3.useCount() == 3 &&
+                s1.get() == s2.get() && s2.get() == s3.get());
 }
 
 TEST(SharedReset, Test1) {
     class Contrainer {};
-    SharedPtr<Contrainer> s1 = MakeShared<Contrainer>();
-    s1.Reset();
+    SharedPtr<Contrainer> s1 = makeShared<Contrainer>();
+    s1.reset();
     ASSERT_FALSE(s1);
 }
 
 TEST(SharedReset, Test2) {
     class Contrainer {};
-    SharedPtr<Contrainer> s1 = MakeShared<Contrainer>();
+    SharedPtr<Contrainer> s1 = makeShared<Contrainer>();
     Contrainer* p = new Contrainer;
-    s1.Reset(p);
+    s1.reset(p);
 
-    ASSERT_TRUE(s1.Get() == p && s1.UseCount() == 1);
+    ASSERT_TRUE(s1.get() == p && s1.useCount() == 1);
 }
 
 TEST(SharedReset, Test3) {
     class Contrainer {};
 
-    SharedPtr<Contrainer> s1 = MakeShared<Contrainer>();
+    SharedPtr<Contrainer> s1 = makeShared<Contrainer>();
     Contrainer* p = new Contrainer;
-    s1.Reset(p, std::default_delete<Contrainer>());
+    s1.reset(p, std::default_delete<Contrainer>());
 
-    ASSERT_TRUE(s1.Get() == p && s1.UseCount() == 1);
+    ASSERT_TRUE(s1.get() == p && s1.useCount() == 1);
 }
 
 TEST(SharedUseCount, Test1) {
     class Contrainer {};
 
-    SharedPtr<Contrainer> s1 = MakeShared<Contrainer>();
+    SharedPtr<Contrainer> s1 = makeShared<Contrainer>();
     SharedPtr<Contrainer> s2 = s1;
     { SharedPtr<Contrainer> s3 = s2; }
 
-    ASSERT_TRUE(s1.UseCount() == 2 && s2.UseCount() == 2 && s1.Get() == s2.Get());
+    ASSERT_TRUE(s1.useCount() == 2 && s2.useCount() == 2 && s1.get() == s2.get());
 }
 
 TEST(SharedSwap, Test1) {
-    SharedPtr<int> s1 = MakeShared<int32_t>(1);
+    SharedPtr<int> s1 = makeShared<int32_t>(1);
     SharedPtr<int> s2 = s1;
-    SharedPtr<int> s3 = MakeShared<int32_t>(2);
-    auto raw_s1 = s1.Get();
-    auto raw_s3 = s3.Get();
-    s1.Swap(s3);
+    SharedPtr<int> s3 = makeShared<int32_t>(2);
+    auto raw_s1 = s1.get();
+    auto raw_s3 = s3.get();
+    s1.swap(s3);
 
-    ASSERT_TRUE(s1.UseCount() == 1 && s2.UseCount() == 2 && s3.UseCount() == 2 &&
-                s2.Get() == s3.Get() && s2.Get() == raw_s1 && s1.Get() == raw_s3);
+    ASSERT_TRUE(s1.useCount() == 1 && s2.useCount() == 2 && s3.useCount() == 2 &&
+                s2.get() == s3.get() && s2.get() == raw_s1 && s1.get() == raw_s3);
 }
 
 TEST(SharedIndirectionOperator, Test1) {
-    SharedPtr<int> s1 = MakeShared<int32_t>(1);
+    SharedPtr<int> s1 = makeShared<int32_t>(1);
     ASSERT_TRUE(*s1 == 1);
 }
 
@@ -139,14 +139,14 @@ TEST(SharedArrowOperator, Test1) {
         }
     };
 
-    SharedPtr<Contrainer> s1 = MakeShared<Contrainer>();
+    SharedPtr<Contrainer> s1 = makeShared<Contrainer>();
     ASSERT_TRUE(s1->Foo() == 1);
 }
 
 TEST(SharedBoolOperator, Test1) {
     class Contrainer {};
 
-    SharedPtr<Contrainer> s1 = MakeShared<Contrainer>();
+    SharedPtr<Contrainer> s1 = makeShared<Contrainer>();
     ASSERT_TRUE(s1);
 }
 
