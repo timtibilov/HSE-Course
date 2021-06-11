6a40dc600556369911d5974469c054d8e0996ebc
t.tibilov <t.tibilov@macbook-t.tibilov>
2021-06-11 19:02:55 +0300

    Implemented type traits

--- module-1/homework/TypeTraits/type_traits/utility.h
+++ module-1/homework/TypeTraits/type_traits/utility.h
@@ -5,28 +5,32 @@
 
 template <typename T>
 struct Uncvref {
-    // Your code goes here
+    using type = std::remove_cv_t<std::remove_reference_t<T>>;
 };
 
 template <typename T>
-using uncvref_t = // Your code goes here
+using uncvref_t = typename Uncvref<T>::type;
 
 template <typename T>
 struct AddConst {
-    using type = // Your code goes here
+    using type = const T;
 };
 
 template <typename T>
-using add_const_t = // Your code goes here
+using add_const_t = typename AddConst<T>::type;
 
 template <typename T>
-struct AddLvalueReference : // Your code goes here
+struct AddLvalueReference {
+    using type = std::remove_reference_t<T>&;
+};
 
 template <typename T>
-struct AddRvalueReference : // Your code goes here
+struct AddRvalueReference {
+    using type = std::remove_reference_t<T>&&;
+};
 
 template <typename T>
-using add_lvalue_reference_t = // Your code goes here
+using add_lvalue_reference_t = typename AddLvalueReference<T>::type;
 
 template <typename T>
-using add_rvalue_reference_t = // Your code goes here
+using add_rvalue_reference_t = typename AddRvalueReference<T>::type;
